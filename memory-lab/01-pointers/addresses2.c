d756c9e07073b8bc45cdfa1b517fea3e9cd9f18e
talhagencc <genc.talha@ibu.edu.mk>
2026-03-28 23:30:58 +0000

    finish

--- memory-lab/01-pointers/addresses2.c
+++ memory-lab/01-pointers/addresses2.c
@@ -1,16 +1,14 @@
 // Stores and prints an integer's address
 // A pointer is a variable that holds a memory address.
 // int *p means "p is a pointer to an int".
-
 #include <stdio.h>
 
 int main(void)
 {
     int n = 50;
+    int *p = &n;
 
-    // TODO: Declare a pointer p that stores the address of n
-    int *p = /* ??? */;
+    printf("%p\n", (void*)p);
 
-    // TODO: Print the address stored in p using %p
-    printf("%p\n", /* ??? */);
+    return 0;
 }
